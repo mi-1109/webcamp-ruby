6214eb01a3e20b915235899a6f4f39d229977822
EC2 Default User <ec2-user@ip-172-31-27-49.ec2.internal>
2021-02-20 06:16:28 +0000

    [Finish]3-7

--- prep-ruby/sample_app/config/initializers/application_controller_renderer.rb
+++ prep-ruby/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '360708fafb8d28b8077de7416f33f5f36d2b5ddd63b18f70b5467da936218f548ab43e8425cb009d5c6d397bae730bd564b421c25dbb28eb4e1d50ea88ea6666'
