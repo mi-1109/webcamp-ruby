680138049706488886a98c4ee7849a3ea4efd163
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-17 06:37:42 +0000

    [Add] 投稿のテスト

---
+++ prep-ruby/sample_app/spec/system/lists_spec.rb
@@ -0,0 +1,20 @@
+#コントローラのテスト
+
+require 'rails_helper'
+
+describe "投稿のテスト" do
+  let!(:list) {create(:list, title:'hoge',body:'body')
+  describe 'トップ画面(top_path)のテスト' do
+    before do
+      visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面に「ここはTopページです」と表示されているか' do
+        expect(page).to have_content('ここはTopページです')
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/top')
+      end
+    end
+  end
+
